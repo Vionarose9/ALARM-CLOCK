0ac497d4a8db2df6ef2c3c14f37a398dbc7edd0c
Viona <117000641+Vionarose9@users.noreply.github.com>
2023-08-13 12:16:35 +0530

    Update DataFlair-Alarm-Clock.py

--- DataFlair-Alarm-Clock.py
+++ DataFlair-Alarm-Clock.py
@@ -1,91 +1,59 @@
-# Import Required Library
+import tkinter as tk #import th required libraries
 from tkinter import *
-import datetime
-import time
-import winsound
+from tkinter import messagebox
+import os,time,winsound
 
-from threading import *
- 
-# Create Object
-root = Tk()
- 
-# Set geometry
-root.geometry("400x200")
- 
-# Use Threading
-def Threading():
-    t1=Thread(target=alarm)
-    t1.start()
- 
-def alarm():
-    # Infinite Loop
-    while True:
-        # Set Alarm
-        set_alarm_time =  f"{hour.get()}:{minute.get()}:{second.get()}"
- 
-        # Wait for one seconds
-        time.sleep(1)
- 
-        # Get current time
-        current_time = datetime.datetime.now().strftime("%H:%M:%S")
-        print(current_time,set_alarm_time)
- 
-        # Check whether set alarm is equal to current time or not
-        if current_time == set_alarm_time:
-            print("Time to Wake up")
-            # Playing sound
-            winsound.PlaySound("sound.wav",winsound.SND_ASYNC)
-            
-            
- 
-# Add Labels, Frame, Button, Optionmenus
-Label(root,text="Alarm Clock",font=("Helvetica 20 bold"),fg="red").pack(pady=10)
-Label(root,text="Set Time", font=("Helvetica 15 bold")).pack()
- 
-frame = Frame(root)
-frame.pack()
- 
-hour = StringVar(root)
-hours = ('00', '01', '02', '03', '04', '05', '06', '07',
-         '08', '09', '10', '11', '12', '13', '14', '15',
-         '16', '17', '18', '19', '20', '21', '22', '23', '24'
-        )
-hour.set(hours[0])
- 
-hrs = OptionMenu(frame, hour, *hours)
-hrs.pack(side=LEFT)
- 
-minute = StringVar(root)
-minutes = ('00', '01', '02', '03', '04', '05', '06', '07',
-           '08', '09', '10', '11', '12', '13', '14', '15',
-           '16', '17', '18', '19', '20', '21', '22', '23',
-           '24', '25', '26', '27', '28', '29', '30', '31',
-           '32', '33', '34', '35', '36', '37', '38', '39',
-           '40', '41', '42', '43', '44', '45', '46', '47',
-           '48', '49', '50', '51', '52', '53', '54', '55',
-           '56', '57', '58', '59', '60')
-minute.set(minutes[0])
- 
-mins = OptionMenu(frame, minute, *minutes)
-mins.pack(side=LEFT)
- 
-second = StringVar(root)
-seconds = ('00', '01', '02', '03', '04', '05', '06', '07',
-           '08', '09', '10', '11', '12', '13', '14', '15',
-           '16', '17', '18', '19', '20', '21', '22', '23',
-           '24', '25', '26', '27', '28', '29', '30', '31',
-           '32', '33', '34', '35', '36', '37', '38', '39',
-           '40', '41', '42', '43', '44', '45', '46', '47',
-           '48', '49', '50', '51', '52', '53', '54', '55',
-           '56', '57', '58', '59', '60')
-second.set(seconds[0])
- 
-secs = OptionMenu(frame, second, *seconds)
-secs.pack(side=LEFT)
- 
-Button(root,text="Set Alarm",font=("Helvetica 15"),command=Threading).pack(pady=20)
- 
-# Execute Tkinter
-root.mainloop()
- 
- 
+create=tk.Tk()#create window
+create.title("ALARM CLOCK")
+create.geometry("400x150")#set geometry
+
+def createWidget():
+    label1=Label(create,text='Enter the time in hh:mm',bg='black',fg='white')
+    label1.grid(row=0,column=0,padx=5,pady=2)
+
+    global entry1
+    entry1=Entry(create,width=10,bg='black',fg='white')#Entry box to enter time in 24 hour format
+    entry1.grid(row=0,column=1)
+
+    label2=Label(create,text='Enter the message of alarm',fg='white',bg='dark blue')
+    label2.grid(row=1,column=0,padx=5,pady=2)
+
+    global entry2
+    entry2=Entry(create,width=25,fg='white',bg='dark blue')#Entry box to input message of alarm
+    entry2.grid(row=1,column=1)
+
+    but=Button(create,text="submit",width=10,command=submit)
+    but.grid(row=2,column=1)
+
+    global label3
+    label3=Label(create,text=" ",bg='green')#empty space to configure the text difference
+    label3.grid(row=4,column=0)
+
+def message1():
+    global entry1,label3
+    Alarm_time_lable=entry1.get()#the alarm time input will be stored
+    label3.config(text='The alarm is counting')
+    messagebox.showinfo('Alarm Clock',f'The Alarm time is{Alarm_time_lable}')
+
+def submit():
+
+    global entry1,entry2,labe3
+    Alarm_time=entry1.get()
+    message1()#calling message function
+    current_time=time.strftime("%H:%M")#gets the current time
+    alarm_message=entry2.get()
+    print('The alarm time is:',Alarm_time)
+    while Alarm_time!=current_time:#infinite loop
+        current_time=time.strftime('%H:%M')
+        time.sleep(1)#waits for one second
+    if Alarm_time==current_time:#checks whether set alarm time is equal to current time
+        print('playing alarm sound')
+        winsound.PlaySound('sound.wav',winsound.SND_ALIAS)
+
+        label3.config(text='Alarm Sound Playing<<<<<<')
+        messagebox.showinfo("Alarm Message",f'the message is:{alarm_message}')
+
+
+createWidget()#call the function
+
+create.mainloop()
